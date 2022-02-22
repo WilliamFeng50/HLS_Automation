f53a7e5da9858e8f4b106633691e723dbef34a38
William Feng <85502968+WilliamFeng50@users.noreply.github.com>
2022-02-22 11:03:16 -0500

    Add files via upload
    basic framework

---
+++ AppConstants.java
@@ -0,0 +1,11 @@
+package testLibs;
+
+public interface AppConstants {
+
+	public static final String url = "https://csan--huat.my.salesforce.com";
+	public static final String userName = "w.feng@hlstherapeutics.com";
+	public static final String passWord = "Cloud@2022";
+	public static final String baseBrowser = "Chrome";
+	
+	
+}
